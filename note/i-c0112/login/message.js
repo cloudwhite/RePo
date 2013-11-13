e517ed5bbe66245a64b920b9f80e00f0577afd48
i-c0112 <a7807118@gmail.com>
2013-11-13 17:14:15 +0800

    layout optimization

--- note/i-c0112/login/message.js
+++ note/i-c0112/login/message.js
@@ -30,8 +30,8 @@ function send_text() {
                    content.val("");
                    load_msg();
                },
-               error: function(jqXHR, status, error){
-                   console.log(data);
+               error: function(jqXHR, status, err){
+                   console.log(err);
                }
             });
 }
