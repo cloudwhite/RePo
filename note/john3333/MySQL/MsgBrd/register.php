17e761f96c63bb2555153f27a4f9d7f0d274bd17
john3333 <sj.john3333@gmail.com>
2013-11-18 01:32:20 +0800

    Update DATA + Simple CSS

--- note/john3333/MySQL/MsgBrd/register.php
+++ note/john3333/MySQL/MsgBrd/register.php
@@ -1,19 +1,21 @@
 <html>
 	<head>
 		<meta charset="utf-8">
+		<link rel="stylesheet" type="css/text" href="./css/register.css">
 		<title>註冊</title>
 	</head>
 	
 	<body>
-		<h1>為了確保日後管理 請先麻煩你/妳先註冊</h1>
-		<form action="./app/register.php" method="post" name="loginform">
-			帳號 : <input type="text" name="user" /><br>
+		<p>請麻煩你/妳先註冊</p>
+		<form action="./app/register.php" method="post" name="registerform" id="registerform">
+			<center>帳號 : <input type="text" name="user" /><br>
 			密碼 : <input type="password" name="password" /><br>
 			暱稱 : <input type="text" name="id" /><br>
 			信箱 : <input type="text" name="email" /><br>
 			電話 : <input type="text" name="phone" /><br>
-			生日 : <input type="text" name="birthday" />
-		<input type="submit" value="註冊">
+			生日 : <input type="text" name="birthday" /><br>
+			<input type="submit" value="註冊"><a href="index.php" style="text-decoration:none">Home Page</a></center>
+			
 		</form>
 	</body>
 </html>
